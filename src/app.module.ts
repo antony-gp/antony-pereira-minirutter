382acae27a51740c56b69c0ea6e57dbe46ca57be
Antony Pereira <antony0000@live.com>
2025-07-20 00:20:01 -0300

    feat: - added database integration with type-orm - added docker compose configuration for development

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OrderProductModule } from './order-product/order-product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [],
+        synchronize: configService.get<string>('NODE_ENV') == 'development',
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
     ProductModule,
     OrderModule,
     OrderProductModule,
